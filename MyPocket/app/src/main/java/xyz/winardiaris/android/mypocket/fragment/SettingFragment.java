0175b958d4d4867761bf4f28b975e03c4577b14f
Aris Winardi <ariswinardy@gmail.com>
2017-05-31 00:46:46 +0700

    add setting fragment

--- MyPocket/app/src/main/java/xyz/winardiaris/android/mypocket/fragment/SettingFragment.java
+++ MyPocket/app/src/main/java/xyz/winardiaris/android/mypocket/fragment/SettingFragment.java
@@ -1,8 +1,22 @@
 package xyz.winardiaris.android.mypocket.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import xyz.winardiaris.android.mypocket.R;
+
 /**
  * Created by ars on 5/30/17.
  */
 
-public class SettingFragment {
+public class SettingFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fr_settings,container,false);
+    }
 }
